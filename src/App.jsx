766b12a86b68156a11da5f9510e0254867df0894
abdul Rafay <abdulrafay700000@gmail.com>
2026-09-23 22:11:51 +0500

    project Repo create

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import BrandSlider from './components/BrandSlider'
+
+const App = () => {
+  return (
+    <div>
+          <Navbar />
+          <Header />
+          <BrandSlider/>
+    </div>
+  )
+}
+
+export default App
